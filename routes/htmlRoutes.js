837fde697f071f23b7d674635a06ec4c1f1e0134
Bobby H <rhoffman103@gmail.com>
2018-10-06 17:50:00 -0400

    working on handlebars pages and routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -25,11 +25,51 @@ module.exports = function(app) {
       db.Posts.findAll({
         include: [db.Authors]
       }).then(function(dbPosts) {
-        console.log("POSTS \n" + JSON.stringify(dbPosts, null, 2))
+        // console.log("POSTS \n" + JSON.stringify(dbPosts, null, 2))
         res.render("blueit", {
-          posts: dbPosts
+          posts: dbPosts,
+          findPosts: true
         });
       });
+  });
+
+    // USERNAME ROUTE. render posts by name
+    app.get("/blueit/:name", function(req, res) {
+      // Here we add an "include" property to our options in our findOne query
+      // We set the value to an array of the models we want to include in a left outer join
+      // In this case, just db.Post
+      db.Authors.findOne({
+        where: {
+          UserName: req.params.name
+        },
+        include: [db.Posts]
+      }).then(function(dbPosts) {
+        // res.json(dbPosts);
+        console.log("logging from htmlRoutes.js");
+        console.log("user name " + req.params.name);
+        // console.log("POSTS ==============================\n" + JSON.stringify(dbPosts.Posts, null, 2));
+        res.render("blueit", {
+          posts: dbPosts,
+          user: req.params.name,
+          findUser: true,
+        })
+      });
+    });
+
+      // RENDER POSTS BY LOCATION
+  app.get("/blueit/posts/:location", (req, res) => {
+    db.Posts.findAll({
+      where: {
+        Location: req.params.location
+      },
+      include: [db.Authors]
+    }).then(dbPosts => {
+      // res.json(dbPosts);
+      res.render("blueit", {
+        posts: dbPosts,
+        postal: true
+      });
+    });
   });
 
   // Render 404 page for any unmatched routes
