ae72a150ded4f28b640717ecd2d38f69dd1a52b3
Scott Sawyer <38329255+scottasawyer@users.noreply.github.com>
2018-10-07 16:05:13 -0400

    Fixed error with redirect on sign up / sign in

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,14 @@
-var authController = require('../controllers/authcontroller');
+var authController = require('../controllers/authcontroller.js');
 
-module.exports = function (app, passport) {
-
+module.exports = function (app) {
     app.get('/signup', authController.signup);
     app.get('/signin', authController.signin);
-    app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect: '/',
-            failureRedirect: '/signup'
-        }
-    ));
-
-    app.get('/', isLoggedIn);
-    app.get('/logout', authController.logout);
-    app.post('/signin', passport.authenticate('local-signin', {
-            successRedirect: '/',
-            failureRedirect: '/signin'
-        }
-    ));
-
-
-    function isLoggedIn(req, res, next) {
-        if (req.isAuthenticated())
-            return next();
-        res.redirect('/signin');
-    }
 }
+
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/dashboard',
+
+    failureRedirect: '/signup'
+}
+
+));
