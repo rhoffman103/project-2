2b6575fc28f06ccc9aa286afd2709470c5792a85
Bobby H <rhoffman103@gmail.com>
2019-02-01 11:47:12 -0500

    moved functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,26 @@
 $(document).ready(function() {
-  // SIDENAV FEATURES
-  $("select").formSelect();
-  $(".sidenav").sidenav();
-  $("input#input_text, textarea#textarea2").characterCounter();
 
-  // A function for handling what happens when the form to create a new post is submitted
-  const handleFormSubmit = () => {
+    // SIDENAV FEATURES
+    $("select").formSelect();
+    $(".sidenav").sidenav();
+    $("input#input_text, textarea#textarea2").characterCounter();
 
-    var userName = $("#user-name").text();
-    var userID;
+    const checkLocationInput = function() {
+        let locationFilter = $("#side-nav-location").val();
+        if (locationFilter.length > 0) {
+            $(location).attr('href',"/posts/" + locationFilter);
+        } else {
+            console.log("no location entered")
+        }
+    }
 
-    // Post Inputs
-    var postBody = $("#textarea2").val();
-    var zip = $("#zip").val();
-    var topics = $("#topics")
-      .parent()
-      .children("input")
-      .val();
+    $("#side-search").on("click", function() {
+        checkLocationInput();
+    })
 
-    $.get("/api/usersearch/" + userName, data => {
-      userID = data[0].ID;
-      console.log("user ID " + userID);
-
-      // Constructing a newPost object to hand to the database
-      var newPost = {
-        Location: zip,
-        Body: postBody,
-        Public: true,
-        AuthorID: userID,
-        Topics: topics
-      };
-      console.log(newPost);
-      submitPost(newPost);
-    });
-  };
-
-  // Submits a new post and brings user to blog page upon completion
-  function submitPost(post) {
-    $.post("/api/addpost", post, function() {
-      console.log("post \n" + post);
-    });
-  }
-
-  $("#submit-post").on("click", function() {
-    handleFormSubmit();
-  });
-
+    $(document).on("keyup", function(event) {
+        if (event.keyCode === 13) {
+            checkLocationInput();
+        }
+    })
 });
